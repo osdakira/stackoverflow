4730b9e2392f43b17396f3c8418115c9263bc15c
Akira Osada <osd.akira@gmail.com>
2016-03-05 15:36:27 +0900

    アップデート

--- 22767/test.rb
+++ 22767/test.rb
@@ -1,7 +1,15 @@
 require "google/api_client"
 require "google_drive"
 
-session = GoogleDrive.saved_session("config.json")
-session.files.each do |file|
-  p file.title
+spreadsheet_key = "1-u-LSZ5KAaiOWbKaaSibVk6-K00LADiCmAgp--ehwQo"
+google_drive_session = GoogleDrive.saved_session("config.json")
+# google_drive_session.files.each do |file|
+#   p file.title
+# end
+
+@worksheet = google_drive_session.spreadsheet_by_key(spreadsheet_key).worksheet_by_title("シート1")
+parent = [["a", "b"], ["c", "d"]]
+parent.each do |child|
+  @worksheet.update_cells(@worksheet.num_rows + 1, 1, [child])
 end
+@worksheet.save
